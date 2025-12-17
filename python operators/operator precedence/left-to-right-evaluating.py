1f755e9da75cf8c631b8a0865c0cda363e74d85d
AikoAii <yaikoaiii@gmail.com>
2025-12-17 19:30:44 +0700

    bitwise operator

---
+++ python operators/operator precedence/left-to-right-evaluating.py
@@ -0,0 +1,3 @@
+#If two operators have the same precedence, the expression is evaluated from left to right.
+#Addition + and subtraction - has the same precedence, and therefore we evaluate the expression from left to right:
+print(5 + 4 - 7 + 3)
